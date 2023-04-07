180059c0ae92ad545127bfd3802b803661275267
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-04-08 03:00:28 +0600

    signup added

---
+++ src/controller/user/userAuth.js
@@ -0,0 +1,70 @@
+const User = require("../../model/userSchema");
+const jwt = require("jsonwebtoken");
+
+//** this function is for generating token */
+function generateToken(id, role) {
+  return (token = jwt.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: "2d",
+  }));
+}
+
+//** signup controller */
+exports.signup = async (req, res, next) => {
+  User.findOne({ email: req.body.email }).then(async (emailAlreadyExists) => {
+    //exec is outdated and .then and .catch is used now alse  .then only gives one props
+
+
+
+    try {
+      if (emailAlreadyExists) {
+        return res.status(400).json({
+          status: "failed",
+          message: "This Email already exists..!!!",
+        });
+      } else {
+        const data = await User.create(req.body);
+
+        // const user = new User(req.body);
+        // const data = await user.save();
+
+        const {
+          _id,
+          firstName,
+          lastName,
+          userName,
+          email,
+          hash_password,
+          profilePicture,
+          contactNumber,
+          role,
+        } = data;
+
+        const token = generateToken(_id, role);
+
+        return res.status(200).send({
+          token: token,
+          user: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            hash_password,
+            profilePicture,
+            contactNumber,
+          },
+        });
+      }
+    } catch (error) {
+
+
+
+      return res.status(400).json({
+        status: "failed",
+        message: "Something went wrong while registering!",
+        error: error,
+      });
+    }
+  });
+};
+
+//! try catch used for solving server crashing while facing any error
